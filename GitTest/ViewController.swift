ab421a5d0afb4cfc3a237c60e6f6aff07a1d9be3
naresh banavath <nareshnayak839@gmail.com>
2020-11-16 14:28:10 +0530

    mastercommit

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Developement under progress")
+        print("mastersadsfa")
         // Do any additional setup after loading the view.
     }
 
